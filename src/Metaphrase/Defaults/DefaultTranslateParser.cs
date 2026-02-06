b62b0e645c9fedf70edb0a3f37a9bf55e27c0d1d
Panos Athanasiou <panos_ath@outlook.com>
2026-02-06 17:08:30 +0200

    refactor: DefaultTranslateParser to be simpler

--- src/Metaphrase/Defaults/DefaultTranslateParser.cs
+++ src/Metaphrase/Defaults/DefaultTranslateParser.cs
@@ -1,5 +1,4 @@
 ﻿using Metaphrase.Abstract;
-using System.Buffers;
 using System.Reflection;
 using System.Text.RegularExpressions;
 
@@ -34,68 +33,25 @@ public sealed partial class DefaultTranslateParser : TranslateParser
         }
 
         var type = parameters.GetType();
-        var props = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
-        if (props.Length == 0)
+        var properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+        if (properties.Length == 0)
         {
             return expr;
         }
 
-        using var buffer = new Buffer(parameters, props);
-        return ParameterRegex().Replace(expr, match =>
+        var result = ParameterRegex().Replace(expr, match =>
         {
-            var defaultValue = match.Value;
             var key = match.Groups[1].Value;
-            var value = buffer.GetOrAdd(key);
-            return value ?? defaultValue;
-        });
-    }
-
-    private readonly struct Buffer : IDisposable
-    {
-        private readonly object parameters;
-        private readonly PropertyInfo[] properties;
-        private readonly KeyValuePair<string, string?>[] buffer;
-
-        public Buffer(object parameters, PropertyInfo[] properties)
-        {
-            this.parameters = parameters;
-            this.properties = properties;
-            buffer = ArrayPool<KeyValuePair<string, string?>>.Shared.Rent(properties.Length);
-        }
-
-        public string? GetOrAdd(string key)
-        {
-            var l = buffer.Length;
             var length = properties.Length;
-            var nextIndex = 0;
-            for (int i = 0; i < length; i++)
+            foreach (var property in properties)
             {
-                if (buffer[i] is { Key: default(string), Value: default(string) })
+                if (property.Name.Equals(key, StringComparison.Ordinal))
                 {
-                    nextIndex = i;
-                    break;
-                }
-                if (buffer[i].Key == key)
-                {
-                    return buffer[i].Value;
+                    return property.GetValue(parameters)?.ToString() ?? match.Value;
                 }
             }
-            for (int i = 0; i < length; i++)
-            {
-                var prop = properties[i];
-                if (prop.Name.Equals(key, StringComparison.Ordinal))
-                {
-                    var value = prop.GetValue(parameters)?.ToString();
-                    buffer[nextIndex] = KeyValuePair.Create(key, value);
-                    return value;
-                }
-            }
-            return null;
-        }
-
-        public void Dispose()
-        {
-            ArrayPool<KeyValuePair<string, string?>>.Shared.Return(buffer);
-        }
+            return match.Value;
+        });
+        return result;
     }
 }
