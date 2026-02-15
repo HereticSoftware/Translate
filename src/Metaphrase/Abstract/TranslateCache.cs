7033100c1410524a67caa31d404a9e4d1bc37bfc
Panos Athanasiou <panos_ath@outlook.com>
2026-02-15 23:51:57 +0200

    add: TranslateCache and Default implementation

---
+++ src/Metaphrase/Abstract/TranslateCache.cs
@@ -0,0 +1,81 @@
+﻿using Metaphrase.Primitives;
+
+namespace Metaphrase.Abstract;
+
+/// <summary>
+/// Class for translation caching. Provides synchronous and asynchronous methods for storing and retrieving translations.
+/// </summary>
+public abstract class TranslateCache
+{
+    /// <summary>
+    /// Get immediately all translations for the specified language.
+    /// </summary>
+    /// <param name="language">The language key.</param>
+    /// <returns>The translations for the specified language.</returns>
+    public abstract Translations Instant(string language);
+
+    /// <summary>
+    /// Get immediately a translation value for the specified language and key.
+    /// </summary>
+    /// <param name="language">The language key.</param>
+    /// <param name="key">The translation key.</param>
+    /// <returns>The translation value for the specified language and key.</returns>
+    public abstract string Instant(string language, string key);
+
+    /// <summary>
+    /// Try to immediately get a translation value for the specified language and key.
+    /// </summary>
+    /// <param name="language">The language key.</param>
+    /// <param name="key">The translation key.</param>
+    /// <param name="value">When this method returns, contains the translation value if found; otherwise, null.</param>
+    /// <returns><see langword="true"/> if the translation was found; otherwise, <see langword="false"/>.</returns>
+    public abstract bool TryInstant(string language, string key, [NotNullWhen(true)] out string? value);
+
+    /// <summary>
+    /// Get the translations for the specified language.
+    /// </summary>
+    /// <param name="language">The language key.</param>
+    /// <returns>The translations for the specified language key.</returns>
+    public abstract Observable<Translations> Get(string language);
+
+    /// <summary>
+    /// Get a translation value for the specified language and key.
+    /// </summary>
+    /// <param name="language">The language key.</param>
+    /// <param name="key">The translation key.</param>
+    /// <returns>An observable that emits the translation value for the specified language and key.</returns>
+    public abstract Observable<string> Get(string language, string key);
+
+    /// <summary>
+    /// Set the translations for the specified language.
+    /// </summary>
+    /// <param name="language">The language key.</param>
+    /// <param name="translations">The translations to set.</param>
+    /// <param name="merge">If <see langword="true"/>, merges the translations with existing ones; otherwise, replaces them.</param>
+    /// <returns>An observable that completes when the operation is finished.</returns>
+    public abstract Observable<Unit> Set(string language, Translations translations, bool merge = false);
+
+    /// <summary>
+    /// Set a translation value for the specified language and key.
+    /// </summary>
+    /// <param name="language">The language key.</param>
+    /// <param name="key">The translation key.</param>
+    /// <param name="value">The translation value to set.</param>
+    /// <returns>An observable that completes when the operation is finished.</returns>
+    public abstract Observable<Unit> Set(string language, string key, string value);
+
+    /// <summary>
+    /// Remove the translations for the specified language.
+    /// </summary>
+    /// <param name="language">The language key.</param>
+    /// <returns>An observable that completes when the operation is finished.</returns>
+    public abstract Observable<Unit> Remove(string language);
+
+    /// <summary>
+    /// Remove a translation value for the specified language and key.
+    /// </summary>
+    /// <param name="language">The language key.</param>
+    /// <param name="key">The translation key.</param>
+    /// <returns>An observable that completes when the operation is finished.</returns>
+    public abstract Observable<Unit> Remove(string language, string key);
+}
---
+++ src/Metaphrase/Defaults/DefaultTranslateCache.cs
@@ -0,0 +1,71 @@
+﻿using Metaphrase.Primitives;
+using System.Runtime.CompilerServices;
+
+namespace Metaphrase.Abstract;
+
+/// <summary>
+/// A default implementation of the <see cref="TranslateCache"/> class that uses an in-memorry store.
+/// </summary>
+public sealed class DefaultTranslateCache : TranslateCache
+{
+    private readonly Languages languages = new();
+
+    /// <inheritdoc/>
+    public override Translations Instant(string language)
+    {
+        return languages.Get(language);
+    }
+
+    /// <inheritdoc/>
+    public override string Instant(string language, string key)
+    {
+        return languages.Get(language).Get(key);
+    }
+
+    /// <inheritdoc/>
+    public override bool TryInstant(string language, string key, [NotNullWhen(true)] out string? value)
+    {
+        Unsafe.SkipInit(out value);
+        return languages.TryGet(language, out var translations) && translations.TryGet(key, out value);
+    }
+
+    /// <inheritdoc/>
+    public override Observable<Translations> Get(string language)
+    {
+        return Observable.Return(Instant(language));
+    }
+
+    /// <inheritdoc/>
+    public override Observable<string> Get(string language, string key)
+    {
+        return Observable.Return(Instant(language, key));
+    }
+
+    /// <inheritdoc/>
+    public override Observable<Unit> Set(string language, Translations translations, bool merge = false)
+    {
+        languages.Set(language, translations, merge);
+        return Observable.Return(Unit.Default);
+    }
+
+    /// <inheritdoc/>
+    public override Observable<Unit> Set(string language, string key, string value)
+    {
+        languages.Get(language).Set(key, value);
+        return Observable.Return(Unit.Default);
+    }
+
+    /// <inheritdoc/>
+    public override Observable<Unit> Remove(string language)
+    {
+        languages.Remove(language);
+        return Observable.Return(Unit.Default);
+    }
+
+    /// <inheritdoc/>
+    public override Observable<Unit> Remove(string language, string key)
+    {
+        languages.Get(language).Remove(key);
+        return Observable.Return(Unit.Default);
+    }
+}
